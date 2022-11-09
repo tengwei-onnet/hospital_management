8217d0549deda3f17b0b8e92b2cd7121a7c055b3
Wong_Teng_Wei <tengwei.wong@on.net.my>
2022-11-09 14:19:27 +0800

    [Justin] Create Hospital Module

---
+++ utar_hospital/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'UTAR Hospital Management',
+    'version': '1.0.0',
+    'category': 'Hospital',
+    'author': 'Mini Project - G1',
+    'sequence': -100,
+    'summary': 'Management Information System',
+    'description': """Hospital Management System""",
+    'depends': [],
+    'data': [
+        'security/ir.model.access.csv', #security file must at first
+        'views/menu.xml',
+        'views/patient_view.xml',
+        'views/appointment_view.xml',
+    ],
+    'demo': [],
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
